8b2a7603a84e6f83d9d530c855ac155ad1e24d82
GlenOttley <glenottley@gmail.com>
2021-05-14 06:27:11 +0100

    moved overflow-x: hidden property on to .app class

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Pricing from "./components/Pricing";
 class App extends React.Component {
 	render() {
 		return (
-			<div>
+			<div className="app">
 				<Background />
 				<main className="main">
 					<Header />
